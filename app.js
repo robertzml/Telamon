5ac0e2343d1f61b814a82bbcd3f9891a20643d47
Robert Office <robertzml@126.com>
2014-07-14 10:47:05 +0800

    Edit template layout. Add dashboard view.
    Signed-off-by: Robert Office <robertzml@126.com>

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var dashboard = require('./routes/dashboard');
+var test = require('./routes/test');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/test', test);
+app.use('/dashboard', dashboard);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
