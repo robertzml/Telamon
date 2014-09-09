59d902d201b95a7080c9cb8114c2282aa797365e
Robert Office <robertzml@126.com>
2014-09-09 15:30:02 +0800

    add check form in dining create.
    Signed-off-by: Robert Office <robertzml@126.com>

--- public/javascripts/telamon.js
+++ public/javascripts/telamon.js
@@ -1,6 +1,50 @@
 
 var telamon = function () { 
 	
+	var handleDiningValidate = function() {
+		var form1 = $('#form_dining');
+		var error1 = $('.alert-danger', form1);
+		var success1 = $('.alert-success', form1);
+
+		form1.validate({
+			errorElement: 'span', //default input error message container
+			errorClass: 'help-block help-block-error', // default input error message class
+			focusInvalid: false, // do not focus the last invalid input			
+			rules: {
+				name: {
+					required: true
+				}
+			},
+
+			invalidHandler: function (event, validator) { //display error alert on form submit              
+				success1.hide();
+				error1.show();
+				Metronic.scrollTo(error1, -200);
+			},
+
+			highlight: function (element) { // hightlight error inputs
+				$(element)
+					.closest('.form-group').addClass('has-error'); // set error class to the control group
+			},
+
+			unhighlight: function (element) { // revert the change done by hightlight
+				$(element)
+					.closest('.form-group').removeClass('has-error'); // set error class to the control group
+			},
+
+			success: function (label) {
+				label
+					.closest('.form-group').removeClass('has-error'); // set success class to the control group
+			},
+
+			submitHandler: function (form) {
+				success1.show();
+				error1.hide();
+				form.submit();
+			}
+		});
+	};
+	
 	
 	var handlePlanningValidate = function() {
 		var form1 = $('#form_planning');
@@ -97,6 +141,10 @@ var telamon = function () {
 		
 		planningValidate: function() {
 			handlePlanningValidate();
+		},
+		
+		diningValidate: function() {
+			handleDiningValidate();
 		},
 		
 		initDatatable: function($dom) {
--- routes/dining.js
+++ routes/dining.js
@@ -1,29 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/*
-var mysql = require('mysql');
-
-var pool  = mysql.createPool({
-    host     : '192.168.56.102',
-    user     : 'root',
-    password : '123456',
-    database:'telamon'
-});*/
 
 var pool = require('../models/pool');
-
-
-var loadList = function(req, res) {
-    var sql = "SELECT * FROM dining";
-
-
-    pool.query(sql, function(err, rows) {
-        if (err) throw err;
-
-        res.render('dining/index', { title: '食堂管理', data: rows });
-    });
-};
 
 
 router.get('/', function(req, res) {
@@ -83,7 +62,6 @@ router.post('/edit', function(req, res) {
 
     pool.query(sql, params,  function(err, rows) {
         if (err) throw err;
-
 
         res.redirect('/dining');
     });
