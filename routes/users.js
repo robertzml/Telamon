8d0ddda5cbb4b7d27924b8041d78574ee2c4e280
robertzml <robertzml@126.com>
2015-02-04 16:42:42 +0800

    add user login page.
    Signed-off-by: robertzml <robertzml@126.com>

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
+
+router.get('/login', function(req, res) {
+
+    res.render('users/login', { title: '用户登录' })
+});
+
+router.post('/login', function(req, res) {
+
+    res.render('index/index', { title: '主页' });
+});
+
 module.exports = router;
